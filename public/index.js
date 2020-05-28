ec723ce12bf3f7cbee524cb97537298aa15627e0
Michael MAtteis <michaelcmatteis@gmail.com>
2020-05-28 17:28:25 -0500

    pushing basic layout with api list

--- public/index.js
+++ public/index.js
@@ -1,2 +1,25 @@
-//stuff
-//more stuff
+"use strict"
+
+window.onload = (
+fetch("https://my-json-server.typicode.com/jubs16/Products/Products")
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+  printPoducts(data)
+}))
+
+function printPoducts(data){
+  data.forEach(function(d){
+    let productPlace = document.createElement("div")
+    let productPicture = document.createElement("img")
+    let button = document.createElement("button")
+    productPicture.src = d.imgUrl
+    productPicture.style.height = '200px'
+    productPicture.style.width = "200px"
+    productPlace.appendChild(button)
+    productPlace.appendChild(productPicture)
+    let pictureHolder = document.getElementById("pictureHolder")
+    pictureHolder.appendChild(productPlace)
+    //pictureHolder.appendChild()
+  })
+}
